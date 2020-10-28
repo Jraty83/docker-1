5aaf3e13706ff349e769e60ab937a6d90bb71be2
Janne Raty <jraty@student.hive.fi>
2020-10-28 13:17:53 +0200

    edited

--- 02_bonus/ex01_2048_game/run.sh
+++ 02_bonus/ex01_2048_game/run.sh
@@ -4,4 +4,4 @@ git clone https://github.com/gabrielecirulli/2048.git
 cp Dockerfile 2048/Dockerfile
 cd 2048
 docker build -t twentyfortyeight .
-docker run -d -p 80:80 twentyfortyeight
+docker run -d --name bonus2 -p 80:80 twentyfortyeight
